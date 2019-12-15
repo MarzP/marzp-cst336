6be802aea40cdb8efbf10a6f3744c48bdfc440d3
EC2 Default User <ec2-user@ip-172-31-28-12.us-east-2.compute.internal>
2019-12-15 22:30:50 +0000

    dashboard works. 20 points to gryffindor

--- public/Final/router.js
+++ public/Final/router.js
@@ -4,7 +4,9 @@ const mysql = require('mysql');
 
 //Dashboard Page
 router.get("/", async function(req, res, next){
-    res.render("../public/Final/views/index");
+    let timeSlots = await getTimeSlots();
+    res.render("../public/Final/views/index", {"timeSlots":timeSlots});
+    
 });//index
 
 //Login Page
@@ -12,6 +14,56 @@ router.get("/", async function(req, res, next){
 //Add Time Slot Page
 
 //Remove Time Slot Page
+
+function getTimeSlots(query) {
+    let conn = dbConnection();
+    return new Promise(function(resolve, reject) {
+        conn.connect(function(err) {
+            if(err) throw err;
+            console.log("in database");
+            let sql=`SELECT dateOfSlot, startTime, endTime, bookedByFirstName, bookedByLastName FROM final_timeslots`;
+            conn.query(sql, function(err,rows,fields) {
+                if(err) throw err;
+                resolve(rows);
+            });
+        });//conn
+    });//promise
+}//getTimeSlots
+
+function convertTime(time) {
+        time = time.split(':'); // convert to array
+
+        // fetch
+        var hours = Number(time[0]);
+        var minutes = Number(time[1]);
+        var seconds = Number(time[2]);
+
+        // calculate
+        var timeValue;
+
+        if (hours > 0 && hours <= 12) {
+            timeValue = "" + hours;
+        } else if (hours > 12) {
+            timeValue = "" + (hours - 12);
+        } else if (hours == 0) {
+            timeValue = "12";
+        }
+
+        timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+        timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
+        timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+
+        return timeValue;
+    }//convertTime
+
+function convertDuration(start, end) {
+    start = start.split(':');
+    end = end.split(':');
+    
+    
+}
+
+
 
 //Building connection to database
 function dbConnection() {
