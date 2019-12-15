3a43ece53a7de204731618f391d9a63b75e7ee2a
EC2 Default User <ec2-user@ip-172-31-28-12.us-east-2.compute.internal>
2019-12-15 21:02:03 +0000

    adding files to Final. Index addTime and removeTime are laid out. Index has table started

--- public/Final/router.js
+++ public/Final/router.js
@@ -2,8 +2,26 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
+//Dashboard Page
 router.get("/", async function(req, res, next){
     res.render("../public/Final/views/index");
 });//index
+
+//Login Page
+
+//Add Time Slot Page
+
+//Remove Time Slot Page
+
+//Building connection to database
+function dbConnection() {
+    let conn = mysql.createConnection({
+        host: "olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "lhgmlbcss59s9m0p",
+        password: "vrf2lrmgmpwe02g5",
+        database: "id9he8k3oeqj35ei"
+    });//createConnection
+    return conn;
+}//dbConnection
 
 module.exports = router;
